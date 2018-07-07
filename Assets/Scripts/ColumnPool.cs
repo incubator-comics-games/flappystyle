b4d7398780cf0f51b847741def03a7347be74312
Aric Huang <arichuang@gmail.com>
2018-07-07 14:48:18 -0700

    Basic game
    Basic game from tutorial

---
+++ Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour {
+
+	private GameObject[] columns;
+	public int columnPoolSize = 2;
+	public GameObject columnPrefab;
+	public float spawnRate = 4f;
+	public float columnMin = -1f;
+	public float columnMax = 3.5f;
+
+	private float spawnXPos = 10f;
+	private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+	private int currentColumn = 0;
+
+	private float timeSinceLastSpawned;
+
+	// Use this for initialization
+	void Start () {
+		timeSinceLastSpawned = spawnRate;
+		columns = new GameObject[columnPoolSize];
+		for (int i = 0; i < columnPoolSize; i++) {
+			columns[i] = (GameObject) Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastSpawned += Time.deltaTime;
+
+		if (GameController.instance.gameOver == false && timeSinceLastSpawned >= spawnRate) {
+			timeSinceLastSpawned = 0;
+			float spawnYPos = Random.Range(columnMin, columnMax);
+			columns[currentColumn].transform.position = new Vector2(spawnXPos, spawnYPos);
+			currentColumn = (currentColumn + 1) % columnPoolSize;
+		}
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController instance;
+	public float scrollSpeed = -1.5f;
+	public float speedUpRate = 0.5f;
+	public int speedUpScore = 2;
+	public Text scoreText;
+	public GameObject gameOverText;
+	public bool gameOver = false;
+
+	public bool debugMode = false;
+
+	private int score = 0;
+
+	// Use this for initialization
+	void Awake () {
+		// Singleton pattern
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gameOver && GetInput()) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+
+	public void BirdScored() {
+		if (gameOver) {
+			return;
+		}
+
+		score++;
+		scoreText.text = "Score: " + score.ToString();
+		if (score % speedUpScore == 0) {
+			SpeedUp();
+			if (debugMode) {
+				scoreText.text = "Speeding up! Speed: " + (-scrollSpeed).ToString() + " " + scoreText.text;
+			}
+		}
+	}
+
+	private void SpeedUp() {
+		scrollSpeed -= speedUpRate;
+	}
+
+	public void BirdDied() {
+		gameOverText.SetActive(true);
+		gameOver = true;
+	}
+
+	public bool GetInput() {
+		return (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space));
+	}
+}
