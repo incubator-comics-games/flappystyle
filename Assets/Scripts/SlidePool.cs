c8d10012f716070cf1d65eadd4f4e58d7565b1f2
Eli Fisher <efisher2892@gmail.com>
2018-07-18 13:30:18 -0700

    Add slide auto generation

---
+++ Assets/Scripts/SlidePool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidePool : MonoBehaviour {
+
+	private GameObject[] slides;
+	public int slidePoolSize = 2;
+	public GameObject slidePrefab;
+	public float spawnRate = 4f;
+	public float slideMin = -1f;
+	public float slideMax = 3.5f;
+
+	private float spawnXPos = 10f;
+	private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+	private int currentSlide = 0;
+	private Quaternion spawnRotation = Quaternion.Euler(0,0,270);
+
+	private float timeSinceLastSpawned;
+
+	//Use this for initialization
+	void Start () {
+		timeSinceLastSpawned = spawnRate;
+		slides = new GameObject[slidePoolSize];
+		for (int i = 0; i < slidePoolSize; i++) {
+			slides[i] = (GameObject) Instantiate(slidePrefab, objectPoolPosition, spawnRotation);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastSpawned += Time.deltaTime;
+
+		if (GameController.instance.gameOver == false && timeSinceLastSpawned >= spawnRate) {
+			timeSinceLastSpawned = 0;
+			float spawnYPos = Random.Range(slideMin, slideMax);
+			slides[currentSlide].transform.position = new Vector2(spawnXPos, spawnYPos);
+			currentSlide = (currentSlide + 1) % slidePoolSize;
+		}
+	}
+}
